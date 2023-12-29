3c9171f2c93b3df000654b7258238afd0fc117c5
Ilona <Ilonaverenich@tut.by>
2023-12-29 23:52:42 +0300

    update

---
+++ src/tasks/Array.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+interface Cars {
+    id: number,
+    brand: string,
+    children?: React.ReactNode|React.ReactNode[];
+}
+
+function Array() {
+    const cars: Cars[] =[{id:1, brand:'BMW'},{id:2, brand:'MAZDA'} ]
+
+  return (
+    <div>
+        <p>Array</p>
+
+    </div>
+  )
+}
+
+export default Array
